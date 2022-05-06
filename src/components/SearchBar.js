2201f5a7072f390fffd9b3bf0627ca4745197edf
Hype-Zhumagaliev <zhumagaliev_szh@mail.ru>
2022-05-07 01:01:52 +0600

    Test
    Test

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const SearchBar = ({ OnSearchSubmit }) => {
+    const [term, setTerm] = useState('');
+
+    const onFormSubmit = event => {
+        event.preventDefault();
+
+        OnSearchSubmit(term);
+    };
+    return(
+            <div>
+                <form onSubmit={onFormSubmit}>
+                    <div className="ui fluid massive icon input">
+                        <input 
+                        type="text" 
+                        placeholder="Find videos..." 
+                        onChange={e => setTerm({term: e.target.value })}>
+                            
+                        </input>
+                        <i className="search icon" />
+                    </div>
+                </form>
+            </div>
+        );
+};
+
+
+export default SearchBar;
